9aca5e96c2a0974c51e9b1ea3d360eb8511e28d1
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2020-02-24 00:11:41 -0600

    Use find to locate JAVA_HOME

--- test/windows_java.sh
+++ test/windows_java.sh
@@ -1,4 +1,4 @@
-echo $TRAVIS_JDK_VERSION
+echo "TRAVIS_JDK_VERSION: "$TRAVIS_JDK_VERSION
 if [ $TRAVIS_OS_NAME == "windows" ]; then
     #From https://travis-ci.community/t/java-support-on-windows/286/7
     #export JAVA_HOME=${JAVA_HOME:-/c/jdk}
@@ -6,6 +6,7 @@ if [ $TRAVIS_OS_NAME == "windows" ]; then
     #choco install openjdk11 -params 'installdir=c:\\jdk' -y
     choco install openjdk11
     #export JAVA_HOME="C:\Program Files\OpenJDK\openjdk-11.0.5_10"
-    export JAVA_HOME=`ls "C:\\Program Files\\OpenJDK"`
+    JAVA_ROOT_DIR='C:\Program Files\OpenJDK\'
+    export JAVA_HOME=`find $JAVA_ROOT_DIR -name "openjdk-11*" | tail -n 1`
 fi
-echo $JAVA_HOME
+echo "JAVA_HOME: "$JAVA_HOME
