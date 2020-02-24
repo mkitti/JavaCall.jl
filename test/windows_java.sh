5abecc140632f3dcb848a67511239df8ce5a725a
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2020-02-23 20:21:31 -0600

    Move if statement to Bash script

--- test/windows_java.sh
+++ test/windows_java.sh
@@ -1,4 +1,6 @@
-# https://travis-ci.community/t/java-support-on-windows/286/7
-export JAVA_HOME=${JAVA_HOME:-/c/jdk}
-export PATH=${JAVA_HOME}/bin:${PATH}
-choco install jdk8 -params 'installdir=c:\\jdk' -y
+if [ $TRAVIS_OS_NAME == "windows"]; then
+    #From https://travis-ci.community/t/java-support-on-windows/286/7
+    export JAVA_HOME=${JAVA_HOME:-/c/jdk}
+    export PATH=${JAVA_HOME}/bin:${PATH}
+    choco install jdk8 -params 'installdir=c:\\jdk' -y
+fi
