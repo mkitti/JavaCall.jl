36056934e030883938113c8ca1625d4ba9370ebd
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2020-02-23 22:17:30 -0600

    openjdk11 defaults, let script modify path

--- test/windows_java.sh
+++ test/windows_java.sh
@@ -1,7 +1,8 @@
 echo $TRAVIS_JDK_VERSION
 if [ $TRAVIS_OS_NAME == "windows" ]; then
     #From https://travis-ci.community/t/java-support-on-windows/286/7
-    export JAVA_HOME=${JAVA_HOME:-/c/jdk}
-    export PATH=${JAVA_HOME}/bin:${PATH}
-    choco install openjdk11 -params 'installdir=c:\\jdk' -y
+    #export JAVA_HOME=${JAVA_HOME:-/c/jdk}
+    #export PATH=${JAVA_HOME}/bin:${PATH}
+    #choco install openjdk11 -params 'installdir=c:\\jdk' -y
+    choco install openjdk11
 fi
