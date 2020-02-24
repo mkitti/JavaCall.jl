4f40b5555fe25399a04e0b6323baada2fdea2d4e
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2020-02-24 01:56:27 -0600

    Streamline travis config, add back linux and mac

--- test/windows_java.sh
+++ test/windows_java.sh
@@ -1,9 +1,8 @@
-echo "TRAVIS_JDK_VERSION: "$TRAVIS_JDK_VERSION
 if [ $TRAVIS_OS_NAME == "windows" ]; then
     #From https://travis-ci.community/t/java-support-on-windows/286/7
     #export JAVA_HOME=${JAVA_HOME:-/c/jdk}
     #export PATH=${JAVA_HOME}/bin:${PATH}
-    #choco install openjdk11 -params 'installdir=c:\\jdk' -y
+    #choco install jdk8 -params 'installdir=c:\\jdk' -y
     choco install openjdk11
     #export JAVA_HOME="C:\Program Files\OpenJDK\openjdk-11.0.5_10"
     JAVA_ROOT_DIR='C:\Program Files\OpenJDK\'
