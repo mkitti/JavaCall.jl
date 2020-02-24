fda5fb764e77120815c930b251fdd71984352ee7
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2020-02-23 21:48:07 -0600

    Restrict testing to windows, switch to jdk11

--- test/windows_java.sh
+++ test/windows_java.sh
@@ -1,7 +1,7 @@
+echo $TRAVIS_JDK_VERSION
 if [ $TRAVIS_OS_NAME == "windows" ]; then
     #From https://travis-ci.community/t/java-support-on-windows/286/7
-    echo $TRAVIS_JDK_VERSION
     export JAVA_HOME=${JAVA_HOME:-/c/jdk}
     export PATH=${JAVA_HOME}/bin:${PATH}
-    choco install jdk8 -params 'installdir=c:\\jdk' -y
+    choco install jdk11 -params 'installdir=c:\\jdk' -y
 fi
