7c3dda56e2d9e282c21425927de2c215259bc08d
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2020-02-24 00:19:21 -0600

    Quote path

--- test/windows_java.sh
+++ test/windows_java.sh
@@ -7,6 +7,6 @@ if [ $TRAVIS_OS_NAME == "windows" ]; then
     choco install openjdk11
     #export JAVA_HOME="C:\Program Files\OpenJDK\openjdk-11.0.5_10"
     JAVA_ROOT_DIR='C:\Program Files\OpenJDK\'
-    export JAVA_HOME=`find $JAVA_ROOT_DIR -name "openjdk-11*" | tail -n 1`
+    export JAVA_HOME=`find "$JAVA_ROOT_DIR" -name "openjdk-11*" | tail -n 1`
 fi
 echo "JAVA_HOME: "$JAVA_HOME
