60c485b99f861aea51992e40137e2dfc6b697061
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2020-02-23 21:57:39 -0600

    Switch to openjdk11

--- test/windows_java.sh
+++ test/windows_java.sh
@@ -3,5 +3,5 @@ if [ $TRAVIS_OS_NAME == "windows" ]; then
     #From https://travis-ci.community/t/java-support-on-windows/286/7
     export JAVA_HOME=${JAVA_HOME:-/c/jdk}
     export PATH=${JAVA_HOME}/bin:${PATH}
-    choco install jdk11 -params 'installdir=c:\\jdk' -y
+    choco install openjdk11 -params 'installdir=c:\\jdk' -y
 fi
